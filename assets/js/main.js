6fb8aa65e2be99230fc0011b952153fc128c1fc2
Valeria de Leon <valery.deleon@gmail.com>
2017-03-26 21:20:48 -0600

    previsualiza

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var textarea = document.getElementById("inputComentario");
+
+//document.addEventListener("click", previsualizar)
+
+function previsualizar(){
+  var previsualizar = document.getElementById("textoPrevisualizado")
+  previsualizar.innerHTML = textarea.value;
+}
+previsualizar();
+
+
+function agregar(){
+
+}
